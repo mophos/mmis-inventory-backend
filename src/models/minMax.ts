6aed4782564b2135391457cbb2a503b28b05f4f5
NongMinty <nutsuke.m@gmail.com>
2018-03-21 17:43:19 +0700

    min-max: save generic planning

--- src/models/minMax.ts
+++ src/models/minMax.ts
@@ -3,10 +3,21 @@ import * as moment from 'moment';
 
 export class MinMaxModel {
 
+  getHeader(knex: Knex, warehouseId: any) {
+    let sql = `
+      select from_stock_date, to_stock_date
+      from mm_generic_planning
+      where warehouse_id = ?
+      and is_active = 'Y'
+      limit 1
+    `;
+    return knex.raw(sql, [warehouseId]);
+  }
+
   getMinMax(knex: Knex, warehouseId: any) {
     let sql = `
       select mp.generic_id, mg.working_code, mg.generic_name, sum(wp.qty) qty, mu.unit_name
-      , gp.use_per_day, gp.safty_stock_day, gp.min_qty, gp.max_qty
+      , gp.use_per_day, gp.safty_stock_day, gp.min_qty, gp.max_qty, mg.primary_unit_id
       from wm_products wp
       join mm_products mp on mp.product_id = wp.product_id
       join mm_generics mg on mg.generic_id = mp.generic_id
@@ -14,6 +25,7 @@ export class MinMaxModel {
       join mm_units mu on mu.unit_id = mg.primary_unit_id
       where wp.warehouse_id = ?
       group by mp.generic_id
+      order by mg.generic_name
     `;
     return knex.raw(sql, [warehouseId]);
   }
@@ -22,7 +34,7 @@ export class MinMaxModel {
     let sql = `
       select mp.generic_id, mg.working_code, mg.generic_name, sum(wp.qty) qty, mu.unit_name 
       , IFNULL(sc.use_total, 0) use_total, IFNULL(CEIL(sc.use_per_day), 0) use_per_day
-      , IFNULL(gp.safty_stock_day, 0) safty_stock_day
+      , IFNULL(gp.safty_stock_day, 0) safty_stock_day, mg.primary_unit_id
       from wm_products wp
       join mm_products mp on mp.product_id = wp.product_id
       join mm_generics mg on mg.generic_id = mp.generic_id
@@ -41,7 +53,37 @@ export class MinMaxModel {
       ) sc on sc.generic_id = mp.generic_id
       where wp.warehouse_id = ?
       group by mp.generic_id
+      order by mg.generic_name
     `;
     return knex.raw(sql, [toDate, fromDate, warehouseId, fromDate, toDate, warehouseId]);
+  }
+
+  saveGenericPlanning(knex: Knex, generics: any, warehouseId: any, fromDate: any, toDate: any) {
+    let sqls = [];
+    generics.forEach(g => {
+      let sql = `
+          INSERT INTO mm_generic_planning (
+            warehouse_id, generic_id, primary_unit_id, min_qty, max_qty
+            , use_per_day, safty_stock_day, from_stock_date, to_stock_date
+          )
+          VALUES (
+            ${warehouseId}, '${g.generic_id}', ${g.primary_unit_id}, ${g.min_qty}, ${g.max_qty}, 
+            ${g.use_per_day}, ${g.safty_stock_day}, '${fromDate}', '${toDate}'
+          )
+          ON DUPLICATE KEY UPDATE
+          primary_unit_id = ${g.primary_unit_id}
+          , min_qty = ${g.min_qty}
+          , max_qty = ${g.max_qty}
+          , use_per_day = ${g.use_per_day}
+          , safty_stock_day = ${g.safty_stock_day}
+          , from_stock_date = '${fromDate}'
+          , to_stock_date = '${toDate}'
+        `;
+      sqls.push(sql);
+    });
+
+    let queries = sqls.join(';');
+
+    return knex.raw(queries);
   }
 }
--- src/routes/minMax.ts
+++ src/routes/minMax.ts
@@ -9,7 +9,23 @@ const router = express.Router();
 
 const model = new MinMaxModel();
 
-router.get('/', co(async (req, res, next) => {
+router.get('/header', co(async (req, res, next) => {
+
+  let db = req.db;
+  let warehouseId = req.decoded.warehouseId;
+
+  try {
+    let rs: any = await model.getHeader(db, warehouseId);
+    res.send({ ok: true, rows: rs[0] });
+  } catch (error) {
+    throw error;
+  } finally {
+    db.destroy();
+  }
+
+}));
+
+router.get('/detail', co(async (req, res, next) => {
 
   let db = req.db;
   let warehouseId = req.decoded.warehouseId;
@@ -45,6 +61,45 @@ router.post('/calculate', co(async (req, res, next) => {
         }
       }
       res.send({ ok: true, rows: rs });
+    } else {
+      res.send({ ok: false, error: 'กรุณาระบุช่วงวันที่สำหรับการคำนวณ' });
+    }
+  } catch (error) {
+    throw error;
+  } finally {
+    db.destroy();
+  }
+
+}));
+
+router.post('/save', co(async (req, res, next) => {
+
+  let db = req.db;
+  let _fromDate = req.body.fromDate;
+  let _toDate = req.body.toDate;
+  let _generics = req.body.generics;
+  let warehouseId = req.decoded.warehouseId;
+
+  try {
+    if (_fromDate && _toDate) {
+      const fromDate = moment(_fromDate).format('YYYY-MM-DD');
+      const toDate = moment(_toDate).format('YYYY-MM-DD');
+
+      let generics: any = [];
+      _generics.forEach(g => {
+        generics.push({
+          generic_id: g.generic_id,
+          primary_unit_id: g.primary_unit_id,
+          min_qty: g.min_qty,
+          max_qty: g.max_qty,
+          use_per_day: g.use_per_day,
+          safty_stock_day: g.safty_stock_day
+        });
+      });
+      
+      await model.saveGenericPlanning(db, generics, warehouseId, fromDate, toDate);
+
+      res.send({ ok: true });
     } else {
       res.send({ ok: false, error: 'กรุณาระบุช่วงวันที่สำหรับการคำนวณ' });
     }
