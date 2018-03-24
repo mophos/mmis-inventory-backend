ac9b3d5a464f39aa93a37cb432a7ac84152eb9de
naphattharawat <naphattharawat@gmail.com>
2018-03-24 23:54:00 +0700

    แก้ไขการแสดงผล ใบจัดเวขภัณฑ์ตามใบเบิก V.2

--- src/app.ts
+++ src/app.ts
@@ -269,7 +269,7 @@ app.use('/min-max', checkAuth, minMaxRoute)
 
 app.use('/setting', checkAuth, settingRoute);
 
-app.use('/', checkAuth, indexRoute);
+app.use('/', indexRoute);
 //temperature
 app.use('/temperature', temperatureRoute);
 
