516d738e6db39a0d0aca89a1235ede777ab751ba
beerIT15kmutnb <beerde789@hotmail.com  git config --global user.email beerde789@hotmail.com>
2018-05-28 00:55:28 +0700

    #3

--- src/routes/basic.ts
+++ src/routes/basic.ts
@@ -384,7 +384,7 @@ router.post('/checkApprove', async (req, res, next) => {
     password = crypto.createHash('md5').update(password).digest('hex');
     const isCheck = await basicModel.checkApprove(db, username, password);
     console.log(isCheck[0]);
-    let rights = isCheck[0].split(',');
+    let rights = isCheck[0].access_right.split(',');
 
     if (_.indexOf(rights, action) > -1) {
       res.send({ ok: true })
