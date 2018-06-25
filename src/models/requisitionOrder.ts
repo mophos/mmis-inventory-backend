994cd04ed8c1ee9a893d647f4534416d7a24dec4
naphattharawat <naphattharawat@gmail.com>
2018-06-25 16:55:58 +0700

    fix bug

--- src/models/requisitionOrder.ts
+++ src/models/requisitionOrder.ts
@@ -690,14 +690,7 @@ export class RequisitionOrderModel {
     select rci.wm_product_id, rci.generic_id, floor(rci.confirm_qty/ug.qty) as confirm_qty,
     ug.qty as conversion_qty,mp.product_name,mp.working_code,wp.lot_no,wp.expired_date,
     mu.unit_name as to_unit_name,mu2.unit_name as from_unit_name,ug.qty as conversion_qty,
-    (
-      select sum(vr.remain_qty) as remain_qty
-        from view_product_reserve as vr
-        
-        where vr.warehouse_id=ro.wm_withdraw and vr.generic_id=rci.generic_id
-  
-        
-      ) as remain_qty
+    wp.qty as small_remain_qty,wp.qty/ug.qty as pack_remain_qty
     from wm_requisition_confirm_items as rci
     inner join wm_requisition_confirms as rc on rci.confirm_id = rc.confirm_id
     inner join wm_requisition_orders as ro on ro.requisition_order_id = rc.requisition_order_id
