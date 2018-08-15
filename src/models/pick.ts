3056bba37700c45bfd9828e5eab31d70195cbf10
nawaphol.p <beer@nawapholps-MacBook-Pro.local>
2018-08-15 10:10:06 +0700

    pick

--- src/models/pick.ts
+++ src/models/pick.ts
@@ -8,5 +8,13 @@ export class PickModel {
         .select('p.*','ww.warehouse_name')
         .join('wm_warehouses as ww','ww.warehouse_id','p.wm_pick')
     }
+    gerReceiveNotPO(knex:Knex){
+        return knex('wm_receives as wr')
+        .select('wr.receive_id','wr.receive_code','wr.receive_date')
+        .where('wr.purchase_order_id',null)
+        .andWhere('wr.is_cancel','N')
+        .andWhere('wr.is_success','N')
+        .andWhere('wr.is_completed','N')
+    }
 
 }
--- src/routes/pick.ts
+++ src/routes/pick.ts
@@ -11,19 +11,27 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/getList', async (req, res, next) => {
-
     let db = req.db;
     try {
       let rs: any = await pickModel.getList(db);
-      console.log(rs);
-      
       res.send({ ok: true, rows: rs });
     } catch (error) {
       res.send({ ok: false, error: error.message });
     } finally {
       db.destroy();
     }
-  
+  });
+
+  router.get('/gerReceiveNotPO', async (req, res, next) => {
+    let db = req.db;
+    try {
+      let rs: any = await pickModel.gerReceiveNotPO(db);
+      res.send({ ok: true, rows: rs });
+    } catch (error) {
+      res.send({ ok: false, error: error.message });
+    } finally {
+      db.destroy();
+    }
   });
 
 export default router;
