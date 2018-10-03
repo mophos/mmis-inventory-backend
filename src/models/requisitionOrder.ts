bb8d2df2aef2f7536bc7f6d58fa9aaea5b66743b
nawaphol.p <beer@nawapholps-MacBook-Pro.local>
2018-10-03 15:11:29 +0700

    check dup approve  req unpaid

--- src/models/requisitionOrder.ts
+++ src/models/requisitionOrder.ts
@@ -920,6 +920,11 @@ export class RequisitionOrderModel {
     let query = sql.join(';');
     return knex.raw(query);
   }
+  checkDuplicatedUnpaidApprove(knex: Knex, unpaidId: any) {
+    return knex('wm_requisition_order_unpaids')
+      .count('* as total')
+      .where('requisition_order_unpaid_id', unpaidId);
+  }
   checkDuplicatedApprove(knex: Knex, requisitionId: any) {
     return knex('wm_requisition_confirms')
       .count('* as total')
--- src/routes/requisition.ts
+++ src/routes/requisition.ts
@@ -1064,7 +1064,6 @@ router.put('/orders/confirm/approve/:confirmId', async (req, res, next) => {
     approveData.approve_date = approveDate;
     approveData.approve_people_id = peopleId;
 
-    let approveDatas = [];
     const checkApprove = await orderModel.checkDuplicatedApprove(db, confirmId);
       if (checkApprove[0].total > 0) {
 
@@ -1275,8 +1274,11 @@ router.post('/unpaid/confirm', async (req, res, next) => {
   objSummary.people_id = peopleId;
   objSummary.confirm_date = confirmDate;
   objSummary.created_at = moment().format('YYYY-MM-DD HH:mm:ss');
-
+  
+  
   try {
+    const checkApprove = await orderModel.checkDuplicatedApprove(db, unpaidId);
+    if (checkApprove[0].total > 0) {
     let rsSummary: any = await orderModel.saveConfirmUnpaid(db, objSummary);
     let orderUnpaidId = rsSummary[0];
 
@@ -1438,6 +1440,9 @@ router.post('/unpaid/confirm', async (req, res, next) => {
     await orderModel.decreaseQty(db, dstProducts);
 
     res.send({ ok: true });
+    } else {
+    res.send({ ok: false, error: 'ไม่พบรายการที่ต้องการอนุมัติ' });
+  }
   } catch (error) {
     console.log(error);
     res.send({ ok: false, error: error.message });
