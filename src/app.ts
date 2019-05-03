16d49a8859f12f07c5e2e48cc0d2e29e5a40b005
peemppk <peemppk5538@gmail.com>
2019-05-03 13:25:44 +0700

    pull

--- src/app.ts
+++ src/app.ts
@@ -210,7 +210,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
       }
     },
     debug: true,
-    acquireConnectionTimeout: 5000
+    acquireConnectionTimeout: 3000000
   });
 
   next();
