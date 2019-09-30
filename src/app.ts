9909c801b6d17c7922ee2b79738651ff90b4eff9
naphattharawat <naphattharawat@gmail.com>
2019-09-30 15:56:39 +0700

    query to query2

--- src/app.ts
+++ src/app.ts
@@ -127,8 +127,27 @@ app.use(cors());
 //   body: true,
 //   loggerFunction: console.error
 // }));
+let reqQuery = (req, res, next) => {
+  const url = decodeURI(req.url).substring(_.indexOf(req.url, '?') + 1, decodeURI(req.url).length).split('&');
+  let query = {};
+  for (const u of url) {
+    const up = u.split('=');
+    query[up[0]] = up[1]
+  }
+  req.query2 = query;
+  next();
+}
 
 let checkAuth = (req, res, next) => {
+
+  const url = decodeURI(req.url).substring(_.indexOf(req.url, '?') + 1, decodeURI(req.url).length).split('&');
+  let query = {};
+  for (const u of url) {
+    const up = u.split('=');
+    query[up[0]] = up[1]
+  }
+  req.query2 = query;
+
   let token: string = null;
   if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
     token = req.headers.authorization.split(' ')[1];
@@ -291,7 +310,7 @@ app.use('/setting', checkAuth, settingRoute);
 app.use('/tools', checkAuth, adminAuth, toolsRoute);
 
 app.use('/api', checkAuth, apiRoute);
-app.use('/', checkAuth, indexRoute);
+app.use('/', checkAuth, reqQuery, indexRoute);
 //temperature
 app.use('/temperature', temperatureRoute);
 
