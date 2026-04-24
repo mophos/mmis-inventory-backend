d1bb21611601fc4c4e1394177f5ebcc0f8782686
peempol <peemppk5538@gmail.com>
2026-04-24 15:11:00 +0700

    DISTRIBUTION WAREHOUSE

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -598,20 +598,24 @@ router.get('/report/receiveOrthorCost/:startDate/:endDate/:warehouseId/:warehous
   let warehouseId = req.params.warehouseId;
   let warehouseName = req.params.warehouseName;
   let dateSetting = req.decoded.WM_STOCK_DATE === 'Y' ? 'view_stock_card_warehouse' : 'view_stock_card_warehouse_date';
-  let receiveTpyeId = Array.isArray(req.query.receiveTpyeId) ? req.query.receiveTpyeId : [req.query.receiveTpyeId];
-  const rawObject = receiveTpyeId[0]; 
-
-  // 2. ดึงเฉพาะ "ค่า" (Values) ออกมาเป็น Array ['1', '2', '3', ...]
-  const idsArray = Object.values(rawObject);
-
-  // 3. แปลงเป็น String คั่นด้วยลูกน้ำ "1,2,3,..."
-  const idsString = idsArray.join(',');  
+  
+  let rawreceiveTpyeId: any = req.query.receiveTpyeId;
+  let receiveTpyeId: any[] = [];
+  if (rawreceiveTpyeId !== undefined && rawreceiveTpyeId !== null) {
+      if (typeof rawreceiveTpyeId === 'object' && !Array.isArray(rawreceiveTpyeId)) {
+          receiveTpyeId = Object.values(rawreceiveTpyeId); 
+      } else if (!Array.isArray(rawreceiveTpyeId)) {
+          receiveTpyeId = [rawreceiveTpyeId]; 
+      } else {
+          receiveTpyeId = rawreceiveTpyeId; 
+      }
+  }  
   
   // warehouseId = warehouseId ? +warehouseId : 'ทุกคลังสินค้า'
   try {
     let hosdetail = await inventoryReportModel.hospital(db);
 
-    let data = await inventoryReportModel.receiveOrthorCost(db, startDate, endDate, warehouseId, idsString, dateSetting);
+    let data = await inventoryReportModel.receiveOrthorCost(db, startDate, endDate, warehouseId, receiveTpyeId, dateSetting);
     let hospitalName = hosdetail[0].hospname;
     //  res.send(data[0])
     let sum = inventoryReportModel.comma(_.sumBy(data[0], (o: any) => { return o.costAmount; }));
@@ -645,7 +649,20 @@ router.get('/report/receiveOrthorCostAccount', wrap(async (req, res, next) => {
   let warehouseId: any = req.query.warehouseId;
   let warehouseName: any = req.query.warehouseName;
   let dateSetting = req.decoded.WM_STOCK_DATE === 'Y' ? 'view_stock_card_warehouse' : 'view_stock_card_warehouse_date';
-  let receiveTpyeId = Array.isArray(req.query.receiveTpyeId) ? req.query.receiveTpyeId : [req.query.receiveTpyeId];
+  // let receiveTpyeId = Array.isArray(req.query.receiveTpyeId) ? req.query.receiveTpyeId : [req.query.receiveTpyeId];
+
+  let rawreceiveTpyeId: any = req.query.receiveTpyeId;
+  let receiveTpyeId: any[] = [];
+  if (rawreceiveTpyeId !== undefined && rawreceiveTpyeId !== null) {
+    if (typeof rawreceiveTpyeId === 'object' && !Array.isArray(rawreceiveTpyeId)) {
+        receiveTpyeId = Object.values(rawreceiveTpyeId); 
+    } else if (!Array.isArray(rawreceiveTpyeId)) {
+        receiveTpyeId = [rawreceiveTpyeId]; 
+    } else {
+        receiveTpyeId = rawreceiveTpyeId; 
+    }
+  }  
+
   // warehouseId = warehouseId ? +warehouseId : 'ทุกคลังสินค้า'
   try {
     let hosdetail = await inventoryReportModel.hospital(db);
@@ -4719,19 +4736,22 @@ router.get('/report/receiveOrthorCost/excel/:startDate/:endDate/:warehouseId/:wa
   let warehouseId = req.params.warehouseId;
   let warehouseName = req.params.warehouseName;
   let dateSetting = req.decoded.WM_STOCK_DATE === 'Y' ? 'view_stock_card_warehouse' : 'view_stock_card_warehouse_date';
-  let receiveTpyeId = Array.isArray(req.query.receiveTpyeId) ? req.query.receiveTpyeId : [req.query.receiveTpyeId];
-  const rawObject = receiveTpyeId[0]; 
-
-  // 2. ดึงเฉพาะ "ค่า" (Values) ออกมาเป็น Array ['1', '2', '3', ...]
-  const idsArray = Object.values(rawObject);
-
-  // 3. แปลงเป็น String คั่นด้วยลูกน้ำ "1,2,3,..."
-  const idsString = idsArray.join(',');  
+  let rawreceiveTpyeId: any = req.query.receiveTpyeId;
+  let receiveTpyeId: any[] = [];
+  if (rawreceiveTpyeId !== undefined && rawreceiveTpyeId !== null) {
+      if (typeof rawreceiveTpyeId === 'object' && !Array.isArray(rawreceiveTpyeId)) {
+          receiveTpyeId = Object.values(rawreceiveTpyeId); 
+      } else if (!Array.isArray(rawreceiveTpyeId)) {
+          receiveTpyeId = [rawreceiveTpyeId]; 
+      } else {
+          receiveTpyeId = rawreceiveTpyeId; 
+      }
+  }
 
   // get tmt data
   let hosdetail = await inventoryReportModel.hospital(db);
 
-  let data = await inventoryReportModel.receiveOrthorCost(db, startDate, endDate, warehouseId, idsString, dateSetting);
+  let data = await inventoryReportModel.receiveOrthorCost(db, startDate, endDate, warehouseId, receiveTpyeId, dateSetting);
   if (!data[0].length) {
     res.render('error404')
   } else {
